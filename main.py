6c227271ba17cc8c864e1961b4f3f6b090fd746a
Luis Villalpando <113930826+Levillalpand1@users.noreply.github.com>
2022-09-19 17:53:01 +0000

    print and added new ways to print lists

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ text1 = "Especially in electronic communications, writing in all caps is equival
 # Take every third character starting from the ninth to the end of the sentence, and print the result.
 
 text2 = "Never trust a computer you can't throw out a window"
-
+print(text2[8::3])
 # Reverses the position of all the characters in the following sentence and displays the result on the screen.
 
 text3 = "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
